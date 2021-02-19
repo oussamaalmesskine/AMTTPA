36295b553871e303d43f5417069647050bbebcf2
Votre nom <barberhb6@gmail.com>
2021-02-19 15:57:42 +0100

    all files

--- stage/js/master.js
+++ stage/js/master.js
@@ -80,3 +80,20 @@
 // // })
 
 // // // <!--end contact form-->
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots :true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
