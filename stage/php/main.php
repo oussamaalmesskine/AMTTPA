a69c8a1560af96cee6125716f8c2ac3a9513f5cf
Votre nom <barberhb6@gmail.com>
2021-02-20 15:00:10 +0100

    add fonts

--- stage/php/main.php
+++ stage/php/main.php
@@ -3,12 +3,13 @@
         //assign Variables
         $user =  filter_var($_POST['username'],FILTER_SANITIZE_STRING);
         $mail =  filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+        $phone =  filter_var($_POST['phone'], FILTER_SANITIZE_NUMBER_INT);
         $msg =  filter_var($_POST['teserea'],FILTER_SANITIZE_STRING);
         //creating aray of errors
         $header = 'From: ' .$mail . '\r\n';
         $mayEmail = 'yassinebouza97@gmail.com' ;
         $mysubject = 'Contact Form';
-        mail($mayEmail , $mysubject , $msg , $header);
+        mail($mayEmail , $mysubject , $msg ,$phone, $header);
 
 }
 ?>
